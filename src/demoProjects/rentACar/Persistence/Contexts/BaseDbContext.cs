3e3caabdf54a59c7caa8c1bbcba824220a4d040e
CERAXLAN <er_aslan95@outlook.com>
2022-08-23 16:15:59 +0300

    Week 2

---
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+       
+
+        public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if (!optionsBuilder.IsConfigured)
+            //    base.OnConfiguring(
+            //        optionsBuilder.UseSqlServer(Configuration.GetConnectionString("SomeConnectionString")));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Brand>(a =>
+            {
+                a.ToTable("Brands").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.Name).HasColumnName("Name");
+            });
+
+
+            #region Seed Datas
+
+            Brand[] brandSeedData = { new(1, "Mercedes"), new(2, "Fiat"), new(3, "Opel") };
+            modelBuilder.Entity<Brand>().HasData(brandSeedData);
+
+            #endregion
+           
+
+
+
+        }
+    }
+}
